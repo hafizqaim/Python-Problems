5ce087ae86814be886c9749ebffef37520810e8b
Muhammad Qaim Ali <researchtycoons@gmail.com>
2025-07-03 16:54:30 +0500

    Code for test_prob2.py

---
+++ Problem2/test_prob2.py
@@ -0,0 +1,19 @@
+# Problem2/test_prob2.py
+import unittest
+from .prob2 import recursive_sum
+
+class TestRecursiveSumFunction(unittest.TestCase):
+    def test_multi_digit_number(self):
+        self.assertEqual(recursive_sum(493193), 2)
+
+    def test_single_digit_number(self):
+        self.assertEqual(recursive_sum(5), 5)
+
+    def test_zero(self):
+        self.assertEqual(recursive_sum(0), 0)
+        
+    def test_negative_number(self):
+        self.assertEqual(recursive_sum(-50), 0)
+
+if __name__ == '__main__':
+    unittest.main()
