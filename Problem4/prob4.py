eff8c747f67394447b1eea84dc08d92257f3192c
hafizqaim <researchtycoons@gmail.com>
2025-07-05 14:42:43 +0500

    Updated the exceptions with proper messages

--- Problem4/prob4.py
+++ Problem4/prob4.py
@@ -1,19 +1,18 @@
 def missing_character(alphaList):
     if not alphaList or len(alphaList) < 2:
-        raise ValueError("input list should contain 
-        at least 2 items")
+        raise ValueError("Input list should contain at least 2 items")
     
     if not all(isinstance(c, str) and len(c) == 1 for c in alphaList):
-        return None
+        raise ValueError("All items in the list must be single characters.")
         
     if not all(c.isalpha() for c in alphaList):
-        return None
+        raise ValueError("All characters must be alphabetic.")
         
     for i in range(len(alphaList) - 1):
         if ord(alphaList[i + 1]) - ord(alphaList[i]) != 1:
             print(f"Orignal Array is: {alphaList}")
             return chr(ord(alphaList[i]) + 1)
-    return None
+    return "None. No missing character found in the sequence."
 
 
 if __name__ == '__main__':
