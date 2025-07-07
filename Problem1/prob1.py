0545461f4a3bda82ec62306dd63986ace287805e
Muhammad Qaim Ali <researchtycoons@gmail.com>
2025-07-07 16:00:37 +0500

    Updating problem prob1.py to use set instead of list to improve efficiency
    Co-authored-by: Omar Salman <omar.salman@arbisoft.com>

--- Problem1/prob1.py
+++ Problem1/prob1.py
@@ -2,17 +2,13 @@ def diff_of_lists(lis1,lis2):
     if lis1 is None or lis2 is None:
         raise ValueError("Input lists cannot be None")
     
-    temp_combined = list(lis1) + list(lis2)
-    if not all(isinstance(i, int) for i in temp_combined):
-        raise TypeError("Only integers are allowed in the lists")
-    
-    if not isinstance(lis1, list):
-        lis1 = list(lis1)
-
-    if not isinstance(lis2, list):
-        lis2 = list(lis2)
-    
-    result = []
+    lis1 = list(lis1)
+    lis2 = set(lis2)
+    if (
+        any(not isinstance(i, int) for i in lis1)
+        or any(not isinstance(i, int) for i in lis2)
+    ):
+        raise TypeError("Only integers are allowed in list2")
 
     result = [i for i in lis1 if i not in lis2]
 
